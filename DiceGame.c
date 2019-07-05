16a68d232b1e203184faa3d86262e1c768f154df
飯島裕樹 <iijimayuuki@wlan-napt-002.u-aizu.ac.jp>
2019-07-05 14:24:32 +0900

    Three Dice

--- DiceGame.c
+++ DiceGame.c
@@ -9,6 +9,7 @@ int main(){
     int i;
     int die1;
     int die2;
+    int die3;
     char name[10];
 
     printf("What is your name?\n");
@@ -19,11 +20,13 @@ int main(){
 
         die1=rand()%6+1;
         die2=rand()%6+1;
+        die3=rand()%6+1;
         printf("\nRolling the dice...\n");
         printf("Die 1: %d\n",die1);
         printf("Die 2: %d\n",die2);
-        printf("Total value: %d\n",die1+die2);
-        if (die1+die2>7)printf("%s win!\n\n",name);
+        printf("Die 3: %d\n",die3);
+        printf("Total value: %d\n",die1+die2+die3);
+        if (die1+die2+die3>11)printf("%s win!\n\n",name);
         else printf("%s lost$\n\n",name);
         return 0;
     
